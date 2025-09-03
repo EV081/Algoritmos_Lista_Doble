8da57bcacbe040f0a151228f163099fd187d0ae3
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-09-03 11:26:35 -0500

    Add files via upload

---
+++ circular_doubly_linked_list.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node {
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+
+    Node(T d) {
+        data = d;
+        next = nullptr;
+        prev = nullptr;
+    }
+    ~Node() = default;
+};
+
+template <typename T>
+class List {
+private:
+    Node<T>* head;
+    Node<T>* tail;
+
+public:
+    List() : head(nullptr), tail(nullptr) {}
+
+    Node<T>* getHead() {
+        return head;
+    }
+
+    T front() {
+        return head->data;
+    }
+
+    T back() {
+        return tail->data;
+    }
+
+    void pushFront(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        newNode->prev = nullptr;
+        newNode->next = head;
+        if (head) head->prev = newNode;
+        head = newNode;
+        if (!tail) tail = newNode;
+    }
+
+    void pushBack(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        newNode->next = nullptr;
+        newNode->prev = tail;
+        if (tail) tail->next = newNode;
+        tail = newNode;
+        if (!head) head = newNode;
+    }
+
+    void popFront() {
+        if(!head) return;
+        if(head == tail) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            return;
+        }
+        Node<T>* temp = head;
+        head = head->next;
+        head->prev = nullptr;
+        delete temp;
+    }
+
+    void popBack() {
+        if(!tail) return;
+        if(head == tail) {
+            delete tail;
+            tail = nullptr;
+            head = nullptr;
+            return;
+        }
+        Node<T>* temp = tail;
+        tail = tail->prev;
+        tail->next = nullptr;
+        delete temp;
+    }
+
+    T operator[](int pos) {
+        Node<T>* temp = head;
+        int i = 0;
+        while(i++ < pos) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    bool empty() {
+        return head == nullptr;
+    }
+
+    int size() {
+        Node<T>* temp = head;
+        int i = 0;
+        while(temp != nullptr) {
+            i++;
+            temp = temp->next;
+        }
+        return i;
+    }
+
+    void clear() {
+        while(head != nullptr) {
+            Node<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void reverse() {
+        Node<T>* current = head;
+        Node<T>* temp = nullptr;
+
+        while (current != nullptr) {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+
+        if (temp != nullptr) {
+            head = temp->prev;
+        }
+
+        tail = head;
+        while (tail != nullptr and tail->next != nullptr) {
+            tail = tail->next;
+        }
+    }
+    template<typename T_>
+    friend ostream& operator<<(ostream& os, const List<T_> &l);
+
+    ~List() {
+        clear();
+    }
+
+};
+
+template<typename T>
+ostream& operator<<(ostream &os, const List<T> &l) {
+    Node<T>* temp = l.head;
+    while (temp != nullptr) {
+        os << temp->data << " ";
+        temp = temp->next;
+    }
+    return os;
+}
+
+int main () {
+    List<int> l1;
+    l1.pushFront(30);
+    l1.pushBack(50);
+    l1.pushFront(20);
+    l1.pushFront(10);
+    l1.pushBack(40);
+
+    cout << "-------------" << endl;
+
+    cout << l1 << "\n";
+
+    cout << "Tamaño de lista: " << l1.size() << endl;
+
+    cout << "El valor en la posicion 2:" << endl;
+    cout << l1[2] << endl;
+
+    l1.reverse();
+    cout << "Reverse: ---" << endl;
+    cout << l1 << "\n";
+
+    return 0;
+}

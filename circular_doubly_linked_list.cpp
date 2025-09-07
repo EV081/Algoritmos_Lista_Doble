71815e249f38712d69bc841c08267e6714362c32
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-09-07 10:18:49 -0500

    Add files via upload

--- circular_doubly_linked_list.cpp
+++ circular_doubly_linked_list.cpp
@@ -82,6 +82,70 @@ public:
         delete temp;
     }
 
+    void insert(int pos, T num) {
+        if (!head) {
+            head = new Node<T>(num);
+            tail = head;
+            return;
+        }
+        Node<T>* nodo = new Node<T>(num);
+        Node<T>* temp = head;
+        int i = 0;
+        while(i++ < pos - 1) {
+            temp = temp->next;
+        }
+
+        if (pos == 0) {
+            nodo->next = head;
+            head->prev = nodo;
+            head = nodo;
+            return;
+        }
+
+        if (temp->next == nullptr) {
+            temp->next = nodo;
+            nodo->prev = temp;
+            tail = nodo;
+            return;
+        }
+
+        nodo->next = temp->next;
+        nodo->prev = temp;
+        temp->next->prev = nodo;
+        temp->next = nodo;
+    }
+
+    void remnove(int pos) {
+
+        if (head == nullptr) {
+            return;
+        }
+
+        Node<T>* temp = head;
+        int i = 0;
+        while(i++ < pos - 1) {
+            temp = temp->next;
+        }
+
+        if (temp == head) {
+            head = temp->next;
+            if (head != nullptr) {
+                head->prev = nullptr;
+            }
+        }
+
+        else if (temp->next == nullptr) {
+            temp->prev->next = nullptr;
+        }
+
+        else {
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+        }
+
+        delete temp;
+    }
+
     T operator[](int pos) {
         Node<T>* temp = head;
         int i = 0;
@@ -166,8 +230,11 @@ int main () {
 
     cout << "Tamaño de lista: " << l1.size() << endl;
 
-    cout << "El valor en la posicion 2:" << endl;
-    cout << l1[2] << endl;
+    cout << "Agregar el 5 en la posicion 4: " <<endl;
+    l1.insert(4,5);
+
+    cout << "El valor en la posicion 4:" << endl;
+    cout << l1[4] << endl;
 
     l1.reverse();
     cout << "Reverse: ---" << endl;
